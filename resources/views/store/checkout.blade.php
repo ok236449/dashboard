b06f4a6456233779f92b3bca8e07e1635d2f9abc
ok236449 <ok236449@email.cz>
2021-09-28 19:01:00 +0200

    Update checkout.blade.php

--- resources/views/store/checkout.blade.php
+++ resources/views/store/checkout.blade.php
@@ -119,7 +119,7 @@
                                         </tr>
                                         <tr>
                                             <th>Quantity:</th>
-                                            <td>$product</td>
+                                            <td>{{$product}}</td>
                                         </tr>
                                         <tr>
                                             <th>Total:</th>
