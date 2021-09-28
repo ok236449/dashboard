88823c17f319f055838499c932d03febb55e7935
ok236449 <ok236449@email.cz>
2021-09-28 17:04:19 +0200

    Update checkout.blade.php

--- resources/views/store/checkout.blade.php
+++ resources/views/store/checkout.blade.php
@@ -115,7 +115,7 @@
                                         </tr>
                                         <tr>
                                             <th>Tax (0%)</th>
-                                            <td>{{(env('TAX_PERCENTAGE'))*($product)+(env('TAX_FIXED'))}}</td>
+                                            <td>{{(env('TAX_PERCENTAGE'))+$product+(env('TAX_FIXED'))}}</td>
                                         </tr>
                                         <tr>
                                             <th>Quantity:</th>
