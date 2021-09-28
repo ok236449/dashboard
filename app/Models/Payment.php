9930cf5e39e2643bda47561720f6226a3d85226a
ok236449 <ok236449@email.cz>
2021-09-28 19:11:30 +0200

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -56,9 +56,4 @@ class Payment extends Model
         $formatter = new NumberFormatter($locale, NumberFormatter::CURRENCY);
         return $formatter->formatCurrency($this->price, $this->currency_code);
     }
-    public function formatTax($locale = 'en_US')
-    {
-        $formatter = new NumberFormatter($locale, NumberFormatter::CURRENCY);
-        return $formatter->formatTax($this->price, $this->currency_code);
-    }
 }
