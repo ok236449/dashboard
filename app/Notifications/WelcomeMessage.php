9a3d7dcc9f6f57b9c51d8b8a7d245583f087e8cc
ok236449 <ok236449@email.cz>
2021-11-01 19:03:06 +0100

    Překlad zapomenuté welcome message

--- app/Notifications/WelcomeMessage.php
+++ app/Notifications/WelcomeMessage.php
@@ -41,17 +41,17 @@ class WelcomeMessage extends Notification implements ShouldQueue
         {
             $AdditionalLine = "";
             if(Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_EMAIL') != 0) {
-                $AdditionalLine .= "Verifying your E-Mail Address will grant you ".Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_EMAIL')." additional " . Configuration::getValueByKey('CREDITS_DISPLAY_NAME') . " <br />";
+                $AdditionalLine .= "Ověřením emailu získáte ".Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_EMAIL')." additional " . Configuration::getValueByKey('CREDITS_DISPLAY_NAME') . " <br />";
             }
             if(Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_EMAIL') != 0) {
-                $AdditionalLine .= "Verifying your Mail will also increase your Server Limit by " . Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_EMAIL') . " <br />";
+                $AdditionalLine .= "Ověřením emailu také zvýšíte možný počet serverů o " . Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_EMAIL') . " <br />";
             }
             $AdditionalLine .="<br />";
             if(Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_DISCORD') != 0) {
-                $AdditionalLine .=  "You can also verify your discord account to get another " . Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_DISCORD') . " " . Configuration::getValueByKey('CREDITS_DISPLAY_NAME') . " <br />";
+                $AdditionalLine .=  "Ověřením discordu získáte " . Configuration::getValueByKey('CREDITS_REWARD_AFTER_VERIFY_DISCORD') . " " . Configuration::getValueByKey('CREDITS_DISPLAY_NAME') . " a roli \"Ověřený\" na našem discord serveru s přístupem do dalších kanálů.<br />";
             }
             if(Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_DISCORD') != 0) {
-                $AdditionalLine .=  "Verifying Discord will also increase your Server Limit by " . Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_DISCORD') . " <br />";
+                $AdditionalLine .=  "Ověřením discordu také zvýšíte možný počet serverů o " . Configuration::getValueByKey('SERVER_LIMIT_REWARD_AFTER_VERIFY_DISCORD') . " <br />";
             }
 
             return $AdditionalLine;
@@ -74,7 +74,7 @@ class WelcomeMessage extends Notification implements ShouldQueue
                   ".WelcomeMessage::AdditionalLines()."
                 </p>
                 <h5>Informace</h5>
-                <p>Tento web můžete používat na správu vašich serverů.<br /> Tyto servery můžete spravovat na našem panelu.<br /> Pokud máte jakýkoliv dotaz, kontaktujte nás prosím.</p>
+                <p>Tento web můžete používat na správu vašich serverů.<br /> Tyto servery tu můžete vytvářet a mazat, do konzole serveru se dostanete použitím našeho panelu.<br /> Pokud máte jakýkoliv dotaz, kontaktujte nás prosím.</p>
                 <p>Doufáme, že budete spokojeni s naším hostingem. Máte li nějaké návrhy na zlepšení, nebojte se dát vědět.</p>
                 <p>S přáním hezkého dne,<br />" . config('app.name', 'Laravel') . "</p>
             ",
