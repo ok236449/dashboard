aded4c5fff905b8745c564b21c0b1e52576d903d
ok236449 <ok236449@email.cz>
2021-11-05 17:00:40 +0100

    required email and discord verification notification only

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -302,25 +302,26 @@
     <!-- Content Wrapper. Contains page content -->
 
     <div class="content-wrapper">
-
-        @if(!Auth::user()->hasVerifiedEmail())
-            @if(Auth::user()->created_at->diffInHours(now(), false) > 1)
-                <div class="alert alert-warning p-2 m-2">
-                    <h5><i class="fas fa-envelope"></i> Varování!</h5>
-                    Ještě nemáte ověřenou emailovou adresu. <a class="text-primary"
-                                                                    href="{{route('verification.send')}}">Klikněte zde pro opětovné zaslání ověřovacího linku.</a> <br>
-                    Prosím kontaktujte podporu v případě, že vám nebyl ověřovací link doručen.
-                </div>
-            @else
-                <div class="alert alert-warning p-2 m-2">
-                    <h5><i class="fas fa-envelope"></i> Varování!</h5>
-                    Na email vám byl zaslán link pro ověření emailové adresy.<br>
-                    Prosím kontaktujte podporu v případě, že vám nebyl ověřovací link doručen.
-                </div>
-            @endif
+        @if(strtolower($force_email_verification) == 'true')
+            @if(!Auth::user()->hasVerifiedEmail())
+                @if(Auth::user()->created_at->diffInHours(now(), false) > 1)
+                    <div class="alert alert-warning p-2 m-2">
+                        <h5><i class="fas fa-envelope"></i> Varování!</h5>
+                        Ještě nemáte ověřenou emailovou adresu. <a class="text-primary"
+                                                                        href="{{route('verification.send')}}">Klikněte zde pro opětovné zaslání ověřovacího linku.</a> <br>
+                       Prosím kontaktujte podporu v případě, že vám nebyl ověřovací link doručen.
+                    </div>
+                @else
+                    <div class="alert alert-warning p-2 m-2">
+                        <h5><i class="fas fa-envelope"></i> Varování!</h5>
+                        Na email vám byl zaslán link pro ověření emailové adresy.<br>
+                        Prosím kontaktujte podporu v případě, že vám nebyl ověřovací link doručen.
+                    </div>
+                @endif
             
+            @endif
         @endif
-        @if(is_null(Auth::user()->discordUser))
+        @if((is_null(Auth::user()->discordUser))&&(strtolower($force_discord_verification) == 'true'))
             <div class="alert alert-warning p-2 m-2">
                 <h5><i class="fab fa-discord mr-2"></i>Varování!</h5>
                 Ještě nemáte ověřený discord. <a class="text-primary"
