472b8721d2240c2b2e980e1d2ba813b9b72308a7
ok236449 <ok236449@email.cz>
2021-12-31 13:36:26 +0100

    make again unsortable

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -90,7 +90,7 @@
                     {data: 'role'},
                     {data: 'email', name: 'users.email'},
                     {data: 'credits' , name : 'users.credits'},
-                    {data: 'usage'},
+                    {data: 'usage' , sortable : false},
                     {data: 'servers' , sortable : false},
                     {data: 'verified' , sortable : false},
                     {data: 'last_seen'},
