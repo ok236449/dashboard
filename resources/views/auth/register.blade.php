032d59740a895225bc6af71c2cb792bc5bca7559
ok236449 <89067167+ok236449@users.noreply.github.com>
2022-01-27 18:39:26 +0100

    VOP

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -96,12 +96,12 @@
 
                     <div class="row">
                         <div class="col-8">
-{{--                            <div class="icheck-primary">--}}
-{{--                                <input type="checkbox" id="agreeTerms" name="terms" value="agree">--}}
-{{--                                <label for="agreeTerms">--}}
-{{--                                    I agree to the <a href="#">terms</a>--}}
-{{--                                </label>--}}
-{{--                            </div>--}}
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="agreeTerms" name="terms" value="agree" required>
+                                <label for="agreeTerms">
+                                    Souhlasím s <a href="https://home.vagonbrei.eu/VOP.pdf">VOP</a>
+                                </label>
+                            </div>
                         </div>
                         <!-- /.col -->
                         <div class="col-4">
