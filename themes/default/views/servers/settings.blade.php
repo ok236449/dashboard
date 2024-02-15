78998f4bafdbb1df78e94faa9d0f36140784df0e
ok236449 <89067167+ok236449@users.noreply.github.com>
2024-02-15 17:25:08 +0100

    enable domains for fabric servers

--- themes/default/views/servers/settings.blade.php
+++ themes/default/views/servers/settings.blade.php
@@ -389,7 +389,7 @@ select{
         <!--if(in_array(Auth::user()->role, ['admin', 'moderator', 'client']))-->
         <p class="ml-3 mb-1" style="color: yellow">{{__("The section below is still in beta. If you encounter any issues, please report them on our Discord.")}}</p>
 
-        @if(($nest_id==1&&in_array($egg_id, [2, 3, 22, 58]))|| //minecraft
+        @if(($nest_id==1&&in_array($egg_id, [2, 3, 22, 26, 58]))|| //minecraft
             $nest_id==8||//web
             ($nest_id==12&&in_array($egg_id, [31, 61])) //discord.js, discord.py
             )
