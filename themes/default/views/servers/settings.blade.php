14372a820ce6069c5c183287346f3a6b750db11a
ok236449 <89067167+ok236449@users.noreply.github.com>
2024-03-19 19:58:28 +0100

    Update settings.blade.php

--- themes/default/views/servers/settings.blade.php
+++ themes/default/views/servers/settings.blade.php
@@ -389,7 +389,7 @@ select{
         <!--if(in_array(Auth::user()->role, ['admin', 'moderator', 'client']))-->
         <p class="ml-3 mb-1" style="color: yellow">{{__("The section below is still in beta. If you encounter any issues, please report them on our Discord.")}}</p>
 
-        @if(($nest_id==1&&in_array($egg_id, [2, 3, 22, 26, 58]))|| //minecraft
+        @if(($nest_id==1&&in_array($egg_id, [1, 2, 3, 22, 26, 58]))|| //minecraft
             $nest_id==8||//web
             ($nest_id==12&&in_array($egg_id, [31, 61])) //discord.js, discord.py
             )
@@ -398,7 +398,7 @@ select{
 
                 <div class="card-header">
                     <div class="d-flex justify-content-between">
-                        <h5 class="card-title"><i class="fas fa-tools mr-2"></i>{{ __('Domain and protection management') }}</h5>
+                        <h5 class="card-title"><i class="fas fa-tools mr-2"></i>{{ __('Domain management') }}</h5>
                     </div>
                 </div>
 
